3e18515385360b683810a2d26ded0e35aaa361f3
shreejam21 <shreejan.mistry@intel.com>
2023-05-02 16:40:58 -0700

    Seperated the Azure Workspace from Cluster

--- versions.tf
+++ versions.tf
@@ -9,12 +9,3 @@ terraform {
   }
 
 }
-
-# Specifying the provider for Azure resources
-provider "azurerm" {
-  features {}
-}
-
-provider "databricks" {
-  host = azurerm_databricks_workspace.az-databricks.workspace_url
-}
